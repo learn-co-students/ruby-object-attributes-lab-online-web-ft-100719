618f3c6e52c37e995bfcf4988ee61def75a2ca91
Prince Charles <princecharles35@yahoo.com>
2019-10-23 19:56:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+ def initialize()
+  @name = name
+ end
+
+ def name
+  @name
+ end
+
+ def name=(new_name)
+  @name = new_name
+ end 
+
+end 
+
+mike = Person.new("mike")
+kanye.name
+"mike"
+mike.name = josh
+
+mike.name
