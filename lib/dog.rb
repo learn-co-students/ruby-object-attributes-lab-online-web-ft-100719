35f42898dd96ca42f3e3aa92748e922d347abc74
Duong Dang <dddangmedia@gmail.com>
2019-11-02 02:20:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(given_name)
+    @name = given_name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def breed=(dog_breed)
+   @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
